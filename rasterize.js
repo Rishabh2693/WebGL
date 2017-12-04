a0c0ea2b80c802776a7e447e7b2459afa9ad520f
rishabh2693 <rishabh2693@gmail.com>
2017-12-04 15:16:43 -0500

    some work

--- rasterize.js
+++ rasterize.js
@@ -279,7 +279,7 @@ function mouseMove(event){
     var y = event.clientY - imageCanvas.getBoundingClientRect().top;
     x = 1 - (x/512);
     y = 1 - (y/512); 
-    console.log(x);
+   
     if(lastX == null||lastY ==null)
     {
       
@@ -295,10 +295,7 @@ function mouseMove(event){
     }
     lastX = x;
     lastY = y;
-   // console.log(crosshair);
-    console.log(lastX + " " +lastY +" "+x);
-    console.log(crosshair.translation);
-        // end if there is a highlighted model
+     // end if there is a highlighted model
    // end rotate model
     
     // set up needed view params
@@ -306,8 +303,50 @@ function mouseMove(event){
    
     
 }
-function mouseUp(){
-    console.log("im here2");
+
+function updateMssileLocation(){
+    var lookAt = vec3.create(), viewRight = vec3.create(), temp = vec3.create(); // lookat, right & temp vectors
+    lookAt = vec3.normalize(lookAt,vec3.subtract(temp,Center,Eye)); // get lookat vector
+    viewRight = vec3.normalize(viewRight,vec3.cross(temp,lookAt,Up)); // get view right vector
+   
+    function translateModel(offset) {
+        vec3.add(setModel.translation,setModel.translation,offset);
+    } // end translate model
+    var setModel = null;
+    for(var i = 0; i< numEllipsoids;i++){
+        //  console.log(inputEllipsoids);
+          if(inputEllipsoids[i].type == 1){
+              setModel = (inputEllipsoids[i]);
+              translateModel(temp,viewRight,-inputEllipsoids[i].velocity_x);
+              translateModel(temp,Up,inputEllipsoids[i].velocity_y);
+          }
+      }  
+}
+var reached = false;
+var upMissiles = [];
+var over = false;
+
+
+function mouseUp(event){
+    if(upMissiles.length==0&&!over){
+        for(var i = 0; i< numEllipsoids;i++){
+          //  console.log(inputEllipsoids);
+            if(inputEllipsoids[i].type == 1)
+                upMissiles.push(inputEllipsoids[i]);
+        }  
+    }
+    console.log("size"+upMissiles.length);
+    var x =  event.clientX - imageCanvas.getBoundingClientRect().left ;
+    var y = event.clientY - imageCanvas.getBoundingClientRect().top;
+    x = 1 - (x/512);
+    y = 1 - (y/512); 
+
+    upMissiles[0].velocity_x = (x-upMissiles[0].x)*0.1;
+    upMissiles[0].velocity_y = (y-upMissiles[0].y)*0.1;
+    upMissiles[0].goal_x = x;
+    upMissiles[0].goal_y = y;
+    console.log(upMissiles[0]);
+    upMissiles.shift();
 }
 var imageCanvas = null;
 // set up the webGL environment
